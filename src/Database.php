73295591a8862d32e31888997f96b09fbdb40aa6
abderrahim ghmouch <abderrahimghmouch47@gmail.com>
2026-01-02 12:07:21 +0100

    connection to database

--- src/Database.php
+++ src/Database.php
@@ -10,7 +10,27 @@ class Database
     private $username;
 
     private $password;
+    
 
+    protected function connection(){
 
+        $this->host="localhost";
+        $this->dbname="KARI";
+        $this->username="abdo";
+        $this->password="abdoabdo";
+
+        try{
+            $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->dbname;
+
+            $connection=new PDO($dsn,$this->username,$this->password);
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $connection;
+
+        }catch(PDOException $e)
+        {
+        echo $e->getMessage(); 
+        }
+
+    }
 
 }
